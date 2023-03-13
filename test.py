fb23d31da2d11725e9492540afeb0f7b3515b3e5
Mary <mashulechka-iva@mail.ru>
2023-03-13 13:36:21 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+# Знакомство с языком Python (семинары)
+# Урок 1. Ввод-Вывод, операторы ветвления
+
+"""" print("!")
+a = 1
+print(type(a))
+
+a = '1'
+print(5 % 2)"""
+
+my_list = [1, 2, 3]
+for i, el in enumerate(my_list):
+    print(i, el)
