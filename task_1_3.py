93539397e80eb571db114ae366a223656cf05319
Mary <mashulechka-iva@mail.ru>
2023-03-20 17:01:08 +0300

    Homework 18.03

---
+++ task_1_3.py
@@ -0,0 +1,28 @@
+"""
+1. Пользователь вводит месяц в виде целого числа от 1 до 12.
+Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+Напишите два варианта решения: через list и через dict.
+
+Пример:
+Введите номер месяца: 10
+Результат через список: Осень
+Результат через словарь: Осень
+"""
+time_seasons_list = ['Зима', 'Весна', 'Лето', 'Осень']
+time_seasons_dict = {1: 'Зима', 2: 'Весна', 3: 'Лето', 4: 'Осень'}
+month = int(input("Введите номер месяца от 1 до 12 "))
+if month == 1 or month == 12 or month == 2:
+    print(time_seasons_dict.get(1))
+    print(time_seasons_list[0])
+elif month == 3 or month == 4 or month == 5:
+    print(time_seasons_dict.get(2))
+    print(time_seasons_list[1])
+elif month == 6 or month == 7 or month == 8:
+    print(time_seasons_dict.get(3))
+    print(time_seasons_list[2])
+
+elif month == 9 or month == 10 or month == 11:
+    print(time_seasons_dict.get(4))
+    print(time_seasons_list[3])
+else:
+    print("Такого месяца не существует")
